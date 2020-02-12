eb60f7e2924396540939f31525a5ed7fc2c554f9
Lidii <lidiiany92@gmail.com>
2020-02-12 12:36:44 -0800

    post a new tour endpoint

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const app = express();
 
+//TO HAVE req.body
+app.use(express.json());
+
 //TOP LEVEL CODE EXECUTED ONCE, READ DATA HERE
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -11,10 +14,31 @@ const tours = JSON.parse(
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: {
       tours: tours
     }
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 const PORT = 8000;
