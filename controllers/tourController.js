acb32a37bba625f583d731d359c4dda4a54ca263
Lidii <lidiiany92@gmail.com>
2020-02-14 11:01:22 -0800

    updating and removing a tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,12 +6,35 @@ const Tour = require('./../models/tourModel');
 // );
 
 // ROUTE HANDLES
-exports.getAllTours = (req, res) => {
-  console.log('Getting all tour data');
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: { tours }
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error
+    });
+  }
 };
 
-exports.getTourById = (req, res) => {
-  console.log('Getting tour with id');
+exports.getTourById = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: { tour }
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error
+    });
+  }
 };
 
 exports.addNewTour = async (req, res) => {
@@ -31,10 +54,37 @@ exports.addNewTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  console.log('Update tour');
+exports.updateTour = async (req, res) => {
+  try {
+    const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: updatedTour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  console.log('Delete tour');
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(201).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
