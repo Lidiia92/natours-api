2a3c056ba27bf03872bff9d15a52029d35a0e4e1
Lidii <lidiiany92@gmail.com>
2020-02-14 14:57:13 -0800

    limiting returned fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,18 +12,35 @@ exports.getAllTours = async (req, res) => {
     //const tours = await Tour.find();
 
     //BUILD QUERY
-    // 1) Removing pagination, sort params out of query strings, because they aren't used to filter the data by name or difficulty params
+    // 1a) Filtering
+    //Removing pagination, sort params out of query strings, because they aren't used to filter the data by name or difficulty params
     const queryCopy = { ...req.query };
     const excludedQueryFields = ['page', 'sort', 'limit', 'fields'];
     excludedQueryFields.forEach(field => delete queryCopy[field]);
 
-    // 2) Filtering with operators
+    // 1b) Filtering with operators
     let queryString = JSON.stringify(queryCopy);
     queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => {
       return `$${match}`;
     }); //finding one of those operators and replacing it with the operator name + $
 
-    const query = Tour.find(JSON.parse(queryString));
+    let query = Tour.find(JSON.parse(queryString));
+
+    // 3) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 4) Limiting the returned fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
 
     //EXECUTED QUERY
     const tours = await query;
