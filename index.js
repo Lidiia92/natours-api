79b098c774cb5b66588b2bff7cf0bf4f8d68230a
Lidii <lidiiany92@gmail.com>
2020-03-10 13:36:29 -0700

    setting up base pug template file

--- index.js
+++ index.js
@@ -77,7 +77,10 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Lidiia'
+  });
 });
 
 app.use('/api/v1/tours', tourRoute);
