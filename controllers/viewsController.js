b32851ea58796bc4d14b9df1bdb82e09d0bcdcf7
Lidii <lidiiany92@gmail.com>
2020-03-17 12:27:12 -0700

    tour detail page start

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -15,8 +15,19 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get the data, for the requested tour(including reviews and guides)
+
+  const tour = await (await Tour.findOne({ slug: req.params.slug })).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  // 2) Build template
+
+  // 3) Render template
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+    tour
+  });
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ const viewsController = require('../controllers/viewsController');
 const router = express.Router();
 
 router.get('/', viewsController.getOverview);
-router.get('/tour', viewsController.getTour);
+router.get('/tours/:slug', viewsController.getTour);
 
 module.exports = router;
