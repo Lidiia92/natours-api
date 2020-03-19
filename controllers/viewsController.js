871ed799b911ab139ec7c58e7c06a38b42650b08
Lidii <lidiiany92@gmail.com>
2020-03-19 12:28:53 -0700

    login start

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -15,7 +15,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data, for the requested tour(including reviews and guides)
 
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
@@ -31,3 +31,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
