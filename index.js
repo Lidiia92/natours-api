c16e1d2dbbe5aa821b98018b1e0614bcb4734751
Lidii <lidiiany92@gmail.com>
2020-03-19 18:05:54 -0700

    added cookie for authorized routes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const errorController = require('./controllers/errorController');
 const AppError = require('./utils/appError');
@@ -44,6 +45,7 @@ app.use('/api', limiter);
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' })); // to parse data from req.body
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -67,6 +69,7 @@ app.use(
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
