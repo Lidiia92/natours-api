81d7645435a78c598c521aea9dfebfb2991a1446
Lidii <lidiiany92@gmail.com>
2020-03-31 12:43:33 -0700

    setting up upload images functionality using multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,6 +20,9 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
+
   // 1) Create a new error if user posts password data
 
   if (req.body.password || req.body.passwordConfirm) {
--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.use(
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.cookies);
+  //console.log(req.cookies);
   next();
 });
 
