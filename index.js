59acd1e3cc053ba9abd9a0c487a014d7d45e61b3
Lidii <lidiiany92@gmail.com>
2020-05-18 16:34:24 -0700

    testing for secure connection

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 const viewRoute = require('./routes/viewRoutes');
 
 const app = express();
+app.enable('trust proxy');
 
 // app.use(function(req, res, next) {
 //   res.header('Access-Control-Allow-Origin', '*');
